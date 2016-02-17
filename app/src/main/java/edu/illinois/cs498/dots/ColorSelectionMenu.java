3f1902ca67a3912402f37769f6914fa4ea9a2489
bpbailey <bpbailey@illinois.edu>
2016-02-17 15:25:06 -0600

    Added fling gesture

--- app/src/main/java/edu/illinois/cs498/dots/ColorSelectionMenu.java
+++ app/src/main/java/edu/illinois/cs498/dots/ColorSelectionMenu.java
@@ -2,8 +2,11 @@ package edu.illinois.cs498.dots;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
@@ -11,21 +14,58 @@ import android.widget.PopupMenu;
 /**
  * Created by Administrator on 2/16/2016.
  */
-public class ColorSelectionMenu implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+public class ColorSelectionMenu extends GestureDetector.SimpleOnGestureListener implements View.OnTouchListener, View.OnClickListener, PopupMenu.OnMenuItemClickListener {
     private Activity activity;
     private Button cSelectionButton;
     private PopupMenu cSelectionMenu;
+    private GestureDetector mDetector;
+
 
     public ColorSelectionMenu(Activity activity) {
         this.activity = activity;
         cSelectionButton = (Button) activity.findViewById(R.id.color_selection_button);
-        cSelectionButton.setOnClickListener(this);
+        //cSelectionButton.setOnClickListener(this);
+        cSelectionButton.setOnTouchListener(this);
+        mDetector = new GestureDetector(activity, this);
 
         cSelectionMenu = new PopupMenu(activity, cSelectionButton);
         MenuInflater inflater = cSelectionMenu.getMenuInflater();
         inflater.inflate(R.menu.color_selection_menu, cSelectionMenu.getMenu());
         cSelectionMenu.setOnMenuItemClickListener(this);
+    }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d("DEBUG", "onFling: " + Float.toString(velocityX) + " " + Float.toString(velocityY));
+
+        if (Math.abs(velocityX) >= Math.abs(velocityY)) {
+            DotsView dots = (DotsView) activity.findViewById(R.id.dots_view);
+            String s = cSelectionButton.getText().toString();
+            if (s.equals("BLACK")) {
+                dots.setColor(Color.RED);
+                cSelectionButton.setText("RED");
+            } else if (s.equals("RED")) {
+                dots.setColor(Color.GREEN);
+                cSelectionButton.setText("GREEN");
+            } else if (s.equals("GREEN")) {
+                dots.setColor(Color.BLUE);
+                cSelectionButton.setText("BLUE");
+            } else if (s.equals("BLUE")) {
+                dots.setColor(Color.BLACK);
+                cSelectionButton.setText("BLACK");
+            } else {
+                dots.setColor(Color.BLACK);
+                cSelectionButton.setText("BLACK");
+            }
+        }
+        return true;
     }
 
     public void onClick(View view) {
